9418e24a66049d1b3c9af0eba995bb1a328fff83
albert <63851587@qq.com>
2018-10-15 18:19:01 +0800

    update

--- src/HttpCommand.php
+++ src/HttpCommand.php
@@ -9,8 +9,8 @@
 namespace rabbit\httpserver;
 
 
-use rabbit\console\Command;
 use rabbit\core\ObjectFactory;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
