3eb97aa246244ab69e45639adb0762a64bde4407
albert <63851587@qq.com>
2018-10-24 15:58:01 +0800

    update

--- src/middleware/StartMiddleware.php
+++ src/middleware/StartMiddleware.php
@@ -13,9 +13,6 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use rabbit\core\ObjectFactory;
-use rabbit\governance\trace\TraceInterface;
-use rabbit\server\AttributeEnum;
 
 /**
  * Class StartMiddleware
@@ -33,12 +30,6 @@ class StartMiddleware implements MiddlewareInterface
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         $response = $handler->handle($request);
-
-        if (($tracer = ObjectFactory::get('tracer', null, false)) !== null) {
-            $data = $request->getAttribute(AttributeEnum::TRACE_ATTRIBUTE);
-            /** @var TraceInterface $tracer */
-            $data && $tracer->release($data['traceId']);
-        }
 
         return $this->handleAccept($request, $response);
     }
