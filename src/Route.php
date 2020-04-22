ef11884c8fd137f8a640e72345c90364f35dd3d7
albert <amuluowin@163.com>
2020-04-22 13:53:38 +0800

    remove unuse code

--- src/Route.php
+++ src/Route.php
@@ -15,42 +15,18 @@ use rabbit\server\ServerDispatcher;
  */
 class Route implements RouteInterface
 {
-    const ROUTE_CLASS = 'route_class';
     /**
      * @var DispatcherInterface
      */
     protected $dispatcher;
-    /** @var array */
-    protected $routes = [];
 
     /**
      * Route constructor.
      * @param array $namespaces
      */
-    public function __construct(ServerDispatcher $dispatcher, array $paths, array $include, string $prefix = '')
+    public function __construct(ServerDispatcher $dispatcher)
     {
         $this->dispatcher = $dispatcher;
-        foreach ($paths as $dir => $remove) {
-            FileHelper::findFiles($dir . '/' . $remove, [
-                'filter' => function ($path) use ($include, $prefix, $remove) {
-                    if (strpos($path, '.php') === false) {
-                        return true;
-                    }
-                    $class = ClassHelper::getClassByString(file_get_contents($path));
-                    $list = explode('\\', $class);
-                    if (array_intersect($list, $include + [$remove])) {
-                        $route = str_replace(
-                            array_merge(['\\', $remove], $include),
-                            ['/'],
-                            $class
-                        );
-                        $route = strtolower($prefix . str_replace('//', '/', $route));
-                        $this->routes[$route] = $class;
-                    }
-                    return false;
-                }
-            ]);
-        }
     }
 
     /**
@@ -60,26 +36,39 @@ class Route implements RouteInterface
      */
     public function handle($server)
     {
-        foreach ($this->routes as $route => $handler) {
-            $server->handle(
-                $route,
-                function (\Swoole\Http\Request $request, \Swoole\Http\Response $response) use ($route, $handler) {
-                    try {
-                        $psrRequest = new Request($request);
-                        if ($route === $request->server['request_uri']) {
-                            $psrRequest->withAttribute(self::ROUTE_CLASS, $handler);
-                        }
-                        $psrResponse = new Response($response);
-                        $this->dispatcher->dispatch($psrRequest, $psrResponse);
-                    } catch (\Throwable $throw) {
-                        /**
-                         * @var ErrorHandlerInterface $errorHandler
-                         */
-                        $errorHandler = getDI('errorHandler');
-                        $errorHandler->handle($throw)->send();
-                    }
-                }
-            );
-        }
+//        foreach ($this->routes as $route => $handler) {
+//            $server->handle(
+//                $route,
+//                function (\Swoole\Http\Request $request, \Swoole\Http\Response $response) use ($route, $handler) {
+//                    try {
+//                        $psrRequest = new Request($request);
+//                        if ($route === $request->server['request_uri']) {
+//                            $psrRequest->withAttribute(self::ROUTE_CLASS, $handler);
+//                        }
+//                        $psrResponse = new Response($response);
+//                        $this->dispatcher->dispatch($psrRequest, $psrResponse);
+//                    } catch (\Throwable $throw) {
+//                        /**
+//                         * @var ErrorHandlerInterface $errorHandler
+//                         */
+//                        $errorHandler = getDI('errorHandler');
+//                        $errorHandler->handle($throw)->send();
+//                    }
+//                }
+//            );
+//        }
+        $server->handle('/', function (\Swoole\Http\Request $request, \Swoole\Http\Response $response) {
+            try {
+                $psrRequest = new Request($request);
+                $psrResponse = new Response($response);
+                $this->dispatcher->dispatch($psrRequest, $psrResponse);
+            } catch (\Throwable $throw) {
+                /**
+                 * @var ErrorHandlerInterface $errorHandler
+                 */
+                $errorHandler = getDI('errorHandler');
+                $errorHandler->handle($throw)->send();
+            }
+        });
     }
 }
