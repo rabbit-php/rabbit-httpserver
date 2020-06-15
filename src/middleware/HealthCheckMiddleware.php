57d5624a82fcde0eeac8cb595fbfc9da72b27d31
albert <amuluowin@163.com>
2020-06-15 17:13:51 +0800

    add healthcheck

---
+++ src/middleware/HealthCheckMiddleware.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+
+namespace rabbit\httpserver\middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use rabbit\core\Context;
+
+/**
+ * Class HealthCheckMiddleware
+ * @package rabbit\httpserver\middleware
+ */
+class HealthCheckMiddleware implements MiddlewareInterface
+{
+    /** @var string */
+    protected $health;
+
+    /**
+     * ReqHandlerMiddleware constructor.
+     * @param string|null $health
+     */
+    public function __construct(string $health = '/health')
+    {
+        $this->health = $health;
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @param RequestHandlerInterface $handler
+     * @return ResponseInterface
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $url = $request->getUri()->getPath();
+        if ($url === $this->health) {
+            return Context::get('response');
+        }
+        return $handler->handle($request);
+    }
+}
